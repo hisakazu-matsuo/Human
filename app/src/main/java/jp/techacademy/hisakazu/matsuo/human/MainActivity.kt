7b511d837ba5219223dde2a443a22c52faf85b30
hisakazu-matsuo <matt@kmd.biglobe.ne.jp>
2020-03-09 17:00:21 +0900

    1段階目の投稿 2回目の投稿

--- app/src/main/java/jp/techacademy/hisakazu/matsuo/human/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hisakazu/matsuo/human/MainActivity.kt
@@ -6,13 +6,19 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+   override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+          setContentView(R.layout.activity_main)
 
-        val human = Human("松尾", 62)      // 名前をポチ、年齢3歳で、Dogのインスタンスを作る
 
-        human.say()
+
+        //val human = Human("松尾", 62)      // 名前を松尾、年齢62歳で、Humanのインスタンスを作る
+       val human1= Human()
+          human1. say()
+
+       val human2= Human()
+          human2. think()
+
 
     }
 }
